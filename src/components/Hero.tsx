e8e6fd9a9dfcd06d9e66a32562e57c279f15b01f
axu5 <321axu123@gmail.com>
2024-05-11 23:57:13 +0300

    fix: fixed main page on medium sized screens

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -70,8 +70,8 @@ export function Hero() {
     <>
       {/* Hero */}
       <div className='flex flex-col justify-center items-center py-10 px-40 h-screen md:h-[66vh]'>
-        <BACKGROUND.GOLDEN_RATIO className='invisible sm:visible md:absolute -z-10 top-[225px] rotate-0 w-[calc(60vh*1.618)] h-[60vh]' />
-        <div className='absolute top-[225px] lg:w-[calc(60vh*1.618)] lg:h-[60vh] grid grid-rows-11 lg:grid-rows-1 grid-cols-1 lg:grid-cols-11 px-6 py-4 lg:rotate-0'>
+        <BACKGROUND.GOLDEN_RATIO className='invisible lg:visible lg:absolute -z-10 top-[225px] rotate-0 w-[calc(60vh*1.618)] h-[60vh]' />
+        <div className='absolute top-[225px] lg:w-[calc(60vh*1.618)] lg:h-[60vh] lg:grid lg:grid-rows-1 grid-cols-1 lg:grid-cols-11 px-6 py-4 lg:rotate-0'>
           <div className='md:col-span-7 p-5'>
             <h1 className='font-semibold uppercase tracking-tight text-6xl'>
               Venla Tuomala
@@ -83,7 +83,7 @@ export function Hero() {
               <BlinkingCursor />
             </div>
           </div>
-          <div className='md:col-span-4'>
+          <div className='lg:col-span-4'>
             <Image
               src='/images/venla.png'
               alt='venla'
