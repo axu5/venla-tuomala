e1763ee8bad9d7f613d4658f8fcdcbe5f7ad446c
axu5 <321axu123@gmail.com>
2024-05-11 13:19:55 +0300

    feat: smooth scroll + footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,12 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { GeistSans } from "geist/font/sans";
 import { cn } from "@/lib/utils";
+import {
+  Instagram,
+  Linkedin,
+  SquareArrowOutUpRight,
+} from "lucide-react";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className='scroll-smooth'>
       <body className={cn(inter.className, GeistSans.variable)}>
         <ThemeProvider
           attribute='class'
@@ -35,6 +41,56 @@ export default function RootLayout({
               </h1>
             </div>
             {children}
+            {/* Footer */}
+            <div className='flex flex-row py-24 '>
+              <div className='w-[25%] border border-black flex justify-center items-center p-5'>
+                <span className='pt-serif-bold text-3xl'>VT</span>
+              </div>
+              <div className='w-[25%] border border-black flex items-center p-5'>
+                <ul>
+                  <li>
+                    <Link href='/#about'>About me</Link>
+                  </li>
+                  <li>
+                    <Link href='/#portfolio'>Portfolio</Link>
+                  </li>
+                  <li>
+                    <Link href='/#contact'>Contact</Link>
+                  </li>
+                </ul>
+              </div>
+              <div className='w-[25%] border border-black flex flex-col p-5'>
+                <span className='text-xl'>Socials</span>
+                <div className='flex flex-col'>
+                  <Link
+                    className='flex flex-row items-center'
+                    target='_blank'
+                    href='https://www.instagram.com/tuomalavenla/'>
+                    Send me a message on
+                    <Instagram className='h-4 w-4 ml-2' />
+                  </Link>
+                  <Link
+                    className='flex flex-row items-center'
+                    target='_blank'
+                    href='https://www.linkedin.com/in/venla-tuomala/'>
+                    Connect with me on{" "}
+                    <Linkedin className='h-4 w-4 ml-2' />
+                  </Link>
+                </div>
+              </div>
+              <div className='w-[25%] border border-black flex flex-col p-5'>
+                <span>
+                  &copy; {new Date().getFullYear()} Venla Tuomala
+                </span>
+                <a
+                  className='flex flex-row items-center'
+                  target='_blank'
+                  href='https://github.com/axu5'>
+                  Made by axu{" "}
+                  <SquareArrowOutUpRight className='h-4 w-4 ml-2' />
+                </a>
+              </div>
+            </div>
           </div>
         </ThemeProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import { ImageCarousel } from "@/components/ImageCarousel";
 import { BACKGROUND } from "@/icons/background";
 import { cn } from "@/lib/utils";
-import { ArrowRight, Dot } from "lucide-react";
+import {
+  ArrowRight,
+  Dot,
+  Instagram,
+  Linkedin,
+  SquareArrowOutUpRight,
+} from "lucide-react";
 import Image from "next/image";
 
 export default function LandingPage() {
@@ -11,11 +17,15 @@ export default function LandingPage() {
       <div className='sticky top-5 bg-goldenish z-10'>
         <nav className='border-t-2 border-b-2 py-2 border-black'>
           <ul className='flex flex-row justify-around'>
-            <li className='uppercase text-2xl font-bold'>About</li>
             <li className='uppercase text-2xl font-bold'>
-              Portfolio
+              <a href='/#about'>About</a>
             </li>
-            <li className='uppercase text-2xl font-bold'>Contact</li>
+            <li className='uppercase text-2xl font-bold'>
+              <a href='/#portfolio'>Portfolio</a>
+            </li>
+            <li className='uppercase text-2xl font-bold'>
+              <a href='/#contact'>Contact</a>
+            </li>
           </ul>
         </nav>
       </div>
@@ -46,7 +56,7 @@ export default function LandingPage() {
         </div>
 
         {/* Accordion */}
-        <div className='flex flex-col items-center'>
+        <div className='flex flex-col items-center' id='portfolio'>
           <h2 className='text-3xl font-bold w-full text-center uppercase font-sans border border-transparent border-y-black my-10 py-2'>
             Portfolio
           </h2>
@@ -54,111 +64,13 @@ export default function LandingPage() {
         <AccordionList />
 
         {/* Said about me */}
-        <div className='flex flex-col items-center'>
+        <div className='flex flex-col items-center' id='testimonials'>
           <h2 className='text-3xl font-bold w-full text-center uppercase font-sans border border-transparent border-y-black my-10 py-2'>
             Testimonials
           </h2>
         </div>
-        <div className='flex flex-col md:grid md:grid-cols-3 px-10 md:px-20 gap-8 grid-rows-[max-content_1fr]'>
-          {/* Events */}
-          <div className='grid grid-cols-subgrid col-span-2'>
-            <div className='col-span-1'>
-              <ImageCarousel
-                images={[
-                  { url: "/images/events/sitsit/FC5A5428.jpg" },
-                  { url: "/images/events/sitsit/FC5A5714.jpg" },
-                  { url: "/images/events/sitsit/FC5A5787.jpg" },
-                  { url: "/images/events/sitsit/FC5A5789.jpg" },
-                  { url: "/images/events/sitsit/FC5A5932.jpg" },
-                ]}
-              />
-            </div>
-            <div>
-              <h3 className='pt-serif-bold text-2xl pt-5 pb-1 md:py-1'>
-                Events
-              </h3>
-              <div className='pt-serif-regular-italic flex flex-row'>
-                18.03.2024 <Dot /> Helsinki, Finland
-              </div>
-              <div>
-                “Venla is amazing hire her hire her hire her hire her
-                hire her hire her hire her hire her hire her hire her
-                hire her hire her hire her hire her hire her hire her
-                hire her”
-              </div>
-              <a
-                href='/events'
-                className='pt-serif-regular-italic hover:underline text-blue-600'>
-                see more...
-              </a>
-            </div>
-          </div>
-
-          {/* Graduations */}
-          <div className='grid grid-rows-subgrid col-span-1 row-span-3'>
-            <div className='col-span-1 row-span-2 grid grid-rows-subgrid'>
-              <ImageCarousel
-                className='row-span-2'
-                images={[
-                  { url: "/images/portraits/HNRI4240.jpg" },
-                  { url: "/images/events/graduation/HNRI4251.jpg" },
-                  { url: "/images/events/graduation/HNRI4565.jpg" },
-                  { url: "/images/events/graduation/HNRI4573.jpg" },
-                ]}
-              />
-            </div>
-            <div>
-              <h3 className='pt-serif-bold text-2xl pt-5 pb-1 md:py-1'>
-                Graduations
-              </h3>
-              <div className='pt-serif-regular-italic flex flex-row'>
-                18.03.2024 <Dot /> Lahti, Finland
-              </div>
-              <div>
-                “Venla is a baddie. If I didn’t have a bf I would date
-                her”
-              </div>
-              <a
-                href='/events/graduation'
-                className='pt-serif-regular-italic hover:underline text-blue-600'>
-                see more...
-              </a>
-            </div>
-          </div>
-
-          {/* Couples Photography */}
-          <div className='grid grid-cols-subgrid col-span-2 row-span-5'>
-            <div className='col-span-1 row-span-2 grid grid-rows-subgrid'>
-              <ImageCarousel
-                images={[
-                  { url: "/images/couples/couple1/VEN11.jpg" },
-                  { url: "/images/couples/couple1/VEN02299.jpg" },
-                ]}
-              />
-            </div>
-            <div className='col-span-1'>
-              <h3 className='pt-serif-bold text-2xl pt-5 pb-1 md:py-1'>
-                Couple&apos;s Photography
-              </h3>
-              <div className='pt-serif-regular-italic flex flex-row'>
-                18.03.2024 <Dot /> Mikkeli, Finland
-              </div>
-              <div>
-                “Hello this is a very good and skilled and talented
-                photographer, Venla is also very fun to hang out with
-                she’s cool like that” - Axu
-              </div>
-              <a
-                href='/couples'
-                className='pt-serif-regular-italic hover:underline text-blue-600'>
-                see more...
-              </a>
-            </div>
-          </div>
-        </div>
+        <Testimonials />
       </main>
-      {/* Footer */}
-      <div className='p-12 m-12'>FOOTER GOES HERE</div>
     </>
   );
 }
@@ -230,6 +142,107 @@ function AccordionItem({
         {text}
         <ArrowRight className='pl-2 w-8 h-4 transition-all group-hover:pl-4' />
       </a>
+    </div>
+  );
+}
+
+function Testimonials() {
+  return (
+    <div className='flex flex-col md:grid md:grid-cols-3 px-10 md:px-20 gap-8 grid-rows-[max-content_1fr]'>
+      {/* Events */}
+      <div className='grid grid-cols-subgrid col-span-2'>
+        <div className='col-span-1'>
+          <ImageCarousel
+            images={[
+              { url: "/images/events/sitsit/FC5A5428.jpg" },
+              { url: "/images/events/sitsit/FC5A5714.jpg" },
+              { url: "/images/events/sitsit/FC5A5787.jpg" },
+              { url: "/images/events/sitsit/FC5A5789.jpg" },
+              { url: "/images/events/sitsit/FC5A5932.jpg" },
+            ]}
+          />
+        </div>
+        <div>
+          <h3 className='pt-serif-bold text-2xl pt-5 pb-1 md:py-1'>
+            Events
+          </h3>
+          <div className='pt-serif-regular-italic flex flex-row'>
+            18.03.2024 <Dot /> Helsinki, Finland
+          </div>
+          <div>
+            “Venla is amazing hire her hire her hire her hire her hire
+            her hire her hire her hire her hire her hire her hire her
+            hire her hire her hire her hire her hire her hire her”
+          </div>
+          <a
+            href='/events'
+            className='pt-serif-regular-italic hover:underline text-blue-600'>
+            see more...
+          </a>
+        </div>
+      </div>
+
+      {/* Graduations */}
+      <div className='grid grid-rows-subgrid col-span-1 row-span-3'>
+        <div className='col-span-1 row-span-2 grid grid-rows-subgrid'>
+          <ImageCarousel
+            className='row-span-2'
+            images={[
+              { url: "/images/portraits/HNRI4240.jpg" },
+              { url: "/images/events/graduation/HNRI4251.jpg" },
+              { url: "/images/events/graduation/HNRI4565.jpg" },
+              { url: "/images/events/graduation/HNRI4573.jpg" },
+            ]}
+          />
+        </div>
+        <div>
+          <h3 className='pt-serif-bold text-2xl pt-5 pb-1 md:py-1'>
+            Graduations
+          </h3>
+          <div className='pt-serif-regular-italic flex flex-row'>
+            18.03.2024 <Dot /> Lahti, Finland
+          </div>
+          <div>
+            “Venla is a baddie. If I didn’t have a bf I would date
+            her”
+          </div>
+          <a
+            href='/events/graduation'
+            className='pt-serif-regular-italic hover:underline text-blue-600'>
+            see more...
+          </a>
+        </div>
+      </div>
+
+      {/* Couples Photography */}
+      <div className='grid grid-cols-subgrid col-span-2 row-span-5'>
+        <div className='col-span-1 row-span-2 grid grid-rows-subgrid'>
+          <ImageCarousel
+            images={[
+              { url: "/images/couples/couple1/VEN11.jpg" },
+              { url: "/images/couples/couple1/VEN02299.jpg" },
+            ]}
+          />
+        </div>
+        <div className='col-span-1'>
+          <h3 className='pt-serif-bold text-2xl pt-5 pb-1 md:py-1'>
+            Couple&apos;s Photography
+          </h3>
+          <div className='pt-serif-regular-italic flex flex-row'>
+            18.03.2024 <Dot /> Mikkeli, Finland
+          </div>
+          <div>
+            “Hello this is a very good and skilled and talented
+            photographer, Venla is also very fun to hang out with
+            she’s cool like that” - Axu
+          </div>
+          <a
+            href='/couples'
+            className='pt-serif-regular-italic hover:underline text-blue-600'>
+            see more...
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
