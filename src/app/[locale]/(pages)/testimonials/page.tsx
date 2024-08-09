0df04a675baaa4a6f103e6ba4ff76a73853b29f0
axu5 <axuissocool@gmail.com>
2024-08-09 16:16:52 +0300

    feat: add testimonials page

--- src/app/[locale]/(pages)/testimonials/page.tsx
+++ src/app/[locale]/(pages)/testimonials/page.tsx
@@ -1,9 +1,64 @@
+import { ImageCarousel } from "@/components/ImageCarousel";
+import { SubTitle } from "@/components/SubTitle";
 import { Title } from "@/components/Title";
+import Image from "next/image";
 
 export default function Testimonials() {
   return (
     <>
-      <Title className='py-20'>Testimonials</Title>;
+      <div className='flex items-center justify-center w-full py-20'>
+        <Title>Testimonials</Title>
+      </div>
+      <main className='flex flex-col gap-y-20 w-[80vw] lg:w-full'>
+        <Testimonial
+          name='Saara'
+          testimonial='Venla is a cool and awesome person and she made me feel so comfortable around the camera!'
+          images={[
+            { url: "/images/portraits/FC5A6755.jpg", alt: "" },
+          ]}
+        />
+        <Testimonial
+          name='Saara'
+          testimonial='Venla is a cool and awesome person and she made me feel so comfortable around the camera!'
+          images={[
+            { url: "/images/portraits/FC5A6755.jpg", alt: "" },
+            { url: "/images/portraits/FC5A6755.jpg", alt: "" },
+            { url: "/images/portraits/FC5A6755.jpg", alt: "" },
+            { url: "/images/portraits/FC5A6755.jpg", alt: "" },
+            { url: "/images/portraits/FC5A6755.jpg", alt: "" },
+          ]}
+        />
+        <Testimonial
+          name='Saara'
+          testimonial='Venla is a cool and awesome person and she made me feel so comfortable around the camera!'
+          images={[
+            { url: "/images/portraits/FC5A6755.jpg", alt: "" },
+          ]}
+        />
+      </main>
     </>
+  );
+}
+
+type TestimonialType = {
+  name: string;
+  testimonial: string;
+  images: { url: string; alt: string }[];
+};
+
+function Testimonial({ name, testimonial, images }: TestimonialType) {
+  return (
+    <div className='gap-y-10 flex flex-col md:grid md:grid-cols-2'>
+      <div className='flex flex-col'>
+        <SubTitle className='font-babas-neue text-5xl'>
+          {name}
+        </SubTitle>
+        <div className='font-ibarra text-2xl'>{testimonial}</div>
+      </div>
+      <ImageCarousel
+        className='mx-auto md:max-w-72'
+        images={images}
+      />
+    </div>
   );
 }
--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import {
-    Carousel,
-    CarouselApi,
-    CarouselContent,
-    CarouselItem,
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
 } from "@/components/ui/carousel";
 import { cn } from "@/lib/utils";
 import { ArrowLeft, ArrowRight, Circle } from "lucide-react";
@@ -12,102 +12,99 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 type ImageCarouselProps = {
-    images: {
-        url: string;
-    }[];
-    alt?: string;
-    className?: string;
+  images: {
+    url: string;
+  }[];
+  alt?: string;
+  className?: string;
 };
 
 export function ImageCarousel({
-    images,
-    className,
-    alt
+  images,
+  className,
+  alt,
 }: ImageCarouselProps) {
-    if (!alt) {
-        alt = "";
+  if (!alt) {
+    alt = "";
+  }
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
     }
-    const [api, setApi] = useState<CarouselApi>();
-    const [current, setCurrent] = useState(0);
-    const [total, setTotal] = useState(0);
 
-    useEffect(() => {
-        if (!api) {
-            return;
-        }
+    setCurrent(api.selectedScrollSnap());
+    setTotal(api.scrollSnapList().length);
 
-        setCurrent(api.selectedScrollSnap());
-        setTotal(api.scrollSnapList().length);
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap());
+    });
+  }, [api]);
 
-        api.on("select", () => {
-            setCurrent(api.selectedScrollSnap());
-        });
-    }, [api]);
-
-    return (
-        <Carousel
-            className={cn(className)}
-            // TODO: add trackpad scrolling for laptops
-            opts={{
-                loop: true,
-            }}
-            setApi={setApi}>
-            <CarouselContent
+  return (
+    <Carousel
+      className={cn(className)}
+      // TODO: add trackpad scrolling for laptops
+      opts={{
+        loop: true,
+      }}
+      setApi={setApi}>
+      <CarouselContent
+        className={cn("content-center shadow-sm", className)}>
+        {images.map((image, i) => {
+          return (
+            <CarouselItem key={image.url} className='p-0'>
+              <Image
+                className='h-full object-cover object-center'
+                src={image.url}
+                alt={alt}
+                width={6969}
+                height={0}
+                priority={i === 0}
+              />
+            </CarouselItem>
+          );
+        })}
+      </CarouselContent>
+      {/* Controls */}
+      <div className='flex flex-row items-center justify-between px-5 -translate-y-10 h-0'>
+        <button
+          className='group cursor-pointer w-10 h-10 flex items-center justify-center'
+          onClick={() => {
+            api?.scrollPrev();
+          }}>
+          <ArrowLeft className='w-8 h-8 rounded-full p-1 bg-gray-300 bg-opacity-70 group-hover:bg-opacity-100 transition-all group-hover:-translate-x-2' />
+        </button>
+        <div className='flex flex-row gap-2'>
+          {new Array(total).fill(null).map((_, i) => {
+            return (
+              <Circle
+                key={i}
                 className={cn(
-                    "content-center rounded shadow-sm hover:shadow-2xl",
-                    className
-                )}>
-                {images.map((image, i) => {
-                    return (
-                        <CarouselItem key={image.url} className='rounded'>
-                            <Image
-                                className='h-full object-cover object-center rounded'
-                                src={image.url}
-                                alt={alt}
-                                width={6969}
-                                height={0}
-                                priority={i === 0}
-                            />
-                        </CarouselItem>
-                    );
-                })}
-            </CarouselContent>
-            {/* Controls */}
-            <div className='flex flex-row items-center gap-16 justify-center -translate-y-10 h-0'>
-                <button
-                    className='group cursor-pointer w-10 h-10 flex items-center justify-center'
-                    onClick={() => {
-                        api?.scrollPrev();
-                    }}>
-                    <ArrowLeft className='w-8 h-8 rounded-full p-1 bg-gray-300 bg-opacity-70 group-hover:bg-opacity-100 transition-all group-hover:-translate-x-2' />
-                </button>
-                <div className='flex flex-row gap-2'>
-                    {new Array(total).fill(null).map((_, i) => {
-                        return (
-                            <Circle
-                                key={i}
-                                className={cn(
-                                    "cursor-pointer w-2 h-2 transition-all fill-[#ccc7] stroke-none",
-                                    {
-                                        "fill-white": i === current,
-                                        "hover:fill-[#fff5]": i !== current,
-                                    }
-                                )}
-                                onClick={() => {
-                                    api?.scrollTo(i);
-                                }}
-                            />
-                        );
-                    })}
-                </div>
-                <button
-                    className='group w-10 h-10 flex items-center justify-center'
-                    onClick={() => {
-                        api?.scrollNext();
-                    }}>
-                    <ArrowRight className='w-8 h-8 rounded-full p-1 bg-gray-300 bg-opacity-70 group-hover:bg-opacity-100 transition-all group-hover:translate-x-2' />
-                </button>
-            </div>
-        </Carousel>
-    );
+                  "cursor-pointer w-2 h-2 transition-all fill-[#ccc7] stroke-none",
+                  {
+                    "fill-white": i === current,
+                    "hover:fill-[#fff5]": i !== current,
+                  }
+                )}
+                onClick={() => {
+                  api?.scrollTo(i);
+                }}
+              />
+            );
+          })}
+        </div>
+        <button
+          className='group w-10 h-10 flex items-center justify-center'
+          onClick={() => {
+            api?.scrollNext();
+          }}>
+          <ArrowRight className='w-8 h-8 rounded-full p-1 bg-gray-300 bg-opacity-70 group-hover:bg-opacity-100 transition-all group-hover:translate-x-2' />
+        </button>
+      </div>
+    </Carousel>
+  );
 }
