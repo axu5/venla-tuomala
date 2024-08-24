ff7d7ff1379b4df6b3c7c8597dd1c92f9ed6be73
axu5 <axuissocool@gmail.com>
2024-08-24 14:47:31 +0300

    add trackers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import { PropsWithChildren } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   icons: {
@@ -8,5 +9,10 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      <Analytics />
+    </>
+  );
 }
